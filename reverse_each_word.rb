517545ed1ebe6082d9331553af576731e9a8330b
Kachel Wittig <kjwittig@gmail.com>
2018-03-13 05:05:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,19 @@
-require 'pry'
+require "pry"
+
+# def reverse_each_word(sentence)
+#   reverse_array = []
+#   arr_sentence = sentence.split
+# 
+#   arr_sentence.each do |x|
+#    reverse_array << x.reverse
+#   end
+#   reverse_array.join(" ")
+# end
 
 def reverse_each_word(sentence)
-  reverse_array = []
   arr_sentence = sentence.split
 
-  arr_sentence.each do |x|
+  arr_sentence.map do |x|
    reverse_array << x.reverse
-  end
-  reverse_array.join(" ")
+ end
 end
