2667cb8effec3598712df098c083677b909cbef1
Kachel Wittig <kjwittig@gmail.com>
2018-03-13 04:55:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,10 +4,11 @@ def reverse_each_word(sentence)
   reverse_array = []
   arr_sentence = sentence.split
 
+  binding.pry
   arr_sentence.each_with_index do |arr, index|
   arr_sentence[index].reverse << reverse_array
   end
-  puts "hello"
-  binding.pry
+
+
   reverse_array
 end
