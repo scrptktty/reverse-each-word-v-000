1974d09f72789a58d87d650612045aa76a041092
Kachel Wittig <kjwittig@gmail.com>
2018-03-13 04:53:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,8 +5,9 @@ def reverse_each_word(sentence)
   arr_sentence = sentence.split
 
   arr_sentence.each_with_index do |arr, index|
-
   arr_sentence[index].reverse << reverse_array
+  binding.pry
   end
+
   reverse_array
 end
